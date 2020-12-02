1735649eb4293929a4ff534a19057282e5604ee7
JcFace <jcv@gmail.com>
2020-12-01 17:01:34 -0800

    Seeds done

--- app/controllers/hair_types_controller.rb
+++ app/controllers/hair_types_controller.rb
@@ -1,2 +1,8 @@
 class HairTypesController < ApplicationController
+
+    # def index
+    #     hair_types = HairType.all
+    #     render json: hair_types, only: [:id, :name], include: {products: {only: [:id, :name, :species, :trainer_id]}}
+    # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,34 @@
 
 require 'faker'
 
+HairTypeProduct.delete_all
+Product.delete_all
+HairType.delete_all
+
+HairType.create(name: "Straight")
+HairType.create(name: "Curved")
+HairType.create(name: "Curvy")
+HairType.create(name: "Wavy")
+HairType.create(name: "Curled")
+HairType.create(name: "Curly")
+HairType.create(name: "Very Curly")
+HairType.create(name: "Tight Curls")
+HairType.create(name: "Kinky")
+HairType.create(name: "Coiled")
+
+30.times do
+    Product.create(name: Faker::Space.star_cluster + " Shampoo")
+end
+
+30.times do
+    Product.create(name: Faker::Space.constellation + " Leave-in")
+end
+
+30.times do
+    Product.create(name: Faker::Space.star + " Conditioner")
+end
+
+90.times do
+    HairTypeProduct.create(hair_type_id: HairType.all.sample.id, product_id: Product.all.sample.id)
+end
+
