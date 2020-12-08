ae49a27248dc5896080fd162142ef861387ca695
JcFace <jcv@gmail.com>
2020-12-08 09:27:21 -0800

    Done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,28 @@
 class ProductsController < ApplicationController
 
+    def index
+        products = Product.all
+        render json: products, only: [:id, :name, :description]
+    end
+
+    def create
+        # debugger
+        # products = Product.all
+        product = Product.create(name: params[:name], img_url: params[:image], description: params[:description], hair_type_id: params[:hair_type_id])
+        render json: product
+    end
+    
+    def update
+        # byebug
+        product = Product.find(params[:id])
+        product.update(description: params[:description])
+        render json: product
+    end
+
+    def destroy
+        product = Product.find(params[:id])
+        product.delete
+        render json: {alert: 'Product has been deleted'}
+    end
     
 end
